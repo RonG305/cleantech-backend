d4bfc58a51c4518adeee10426ed173f092807100
Ronald Mutia <mutiaronald138@gmail.com>
2026-01-02 14:15:16 +0300

    feat/auth-setup

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1/auth-service');
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  })
+
+   const config = new DocumentBuilder()
+    .setTitle('Cleantech authservice backend')
+    .setDescription('Cleantech authservice backend API documentation')
+    .setVersion('1.0')
+    .addTag('authservice')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/auth-service/docs', app, documentFactory);
+
+    await app.listen(4001, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}/api/v1/auth-service`);
+  console.log(`Swagger is running on: ${await app.getUrl()}/api/v1/auth-service/docs`);
 }
 bootstrap();
